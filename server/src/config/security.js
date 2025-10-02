22b6dca6f660d53a95e3895681f02ba0a0c404e4
Pasindu shanuka <143288490+PShanuKa@users.noreply.github.com>
2025-10-02 20:32:28 +0530

    up

--- server/src/config/security.js
+++ server/src/config/security.js
@@ -19,7 +19,16 @@ const devOrigins = [
   'http://localhost:5178', 'http://127.0.0.1:5178',
 ]
 
-const allowedOrigins = Array.from(new Set([ ...envOrigins, ...(isProd ? [] : devOrigins) ]))
+const prodOrigins = [
+  'http://localhost:5173', 'http://127.0.0.1:5173',
+  'http://localhost:5174', 'http://127.0.0.1:5174',
+  'http://localhost:5175', 'http://127.0.0.1:5175',
+  'http://localhost:5176', 'http://127.0.0.1:5176',
+  'http://localhost:5177', 'http://127.0.0.1:5177',
+  'http://localhost:5178', 'http://127.0.0.1:5178',
+]
+
+const allowedOrigins = Array.from(new Set([ ...envOrigins, ...(isProd ? prodOrigins : devOrigins) ]))
 
 export const corsOptions = {
   origin: function (origin, callback) {
